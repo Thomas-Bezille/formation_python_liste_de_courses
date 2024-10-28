179343c668d193ba2d442639a6d20eee4506a911
thomas <bezille.thomasb@gmail.com>
2024-10-28 16:40:42 +0100

    📦 NEW: Add an item

---
+++ shopping_list.py
@@ -0,0 +1,11 @@
+shopping_list = []
+
+while True:
+    choice = input("Choisissez parmi les 5 options suivantes :\n1: Ajouter un article à la liste\n2: Supprimer un article de la liste\n3: Afficher la liste\n4: Vider la liste\n5: Quitter\n👉 Votre choix : ")
+    
+    match choice:
+        case "1":
+            item = input("Quel article souhaitez-vous ajouter à la liste ? ")
+            shopping_list.append(item)
+            print(f"{item} a été ajouté à la liste.\n---------------------------------")
+        
